956755730b6fe24c9a0de0f307b617788db4326c
Capucine Dausse <capucine.dausse@etu.unistra.fr>
2025-05-23 23:52:26 +0200

    ajout trucs

--- 24hi/src/Dep.java
+++ 24hi/src/Dep.java
@@ -1,2 +1,131 @@
 public class Dep {
+    private int _base;
+    private int _pos1;
+    private int _pos2;
+
+    public Dep() {
+    }
+
+    public int depD(int posAct, int j)
+    {
+        _base = posAct;
+        _pos2 = j;
+        int temps = 0;
+
+
+        if (_base < _pos2)
+        {
+            while (_base < _pos2)
+            {
+                temps += 4;
+                _base++;
+            }
+        }
+        else if (_base > _pos2)
+        {
+            while (_base > _pos2)
+            {
+                temps += 2;
+                _base--;
+            }
+        }
+        else if (_base == _pos2)
+        {
+            temps += 1;
+        }
+
+        return temps;
+    }
+
+    public int depB(int posAct, int i) {
+        _base = posAct;
+        _pos1 = i;
+
+        int temps = 0;
+
+
+        if (_base < _pos1) {
+            while (_base < _pos1) {
+                temps += 4;
+                _base++;
+            }
+        } else if (_base > _pos1) {
+            while (_base > _pos1) {
+                temps += 2;
+                _base--;
+            }
+        } else if (_base == _pos1) {
+            temps += 1;
+        }
+
+        return temps;
+    }
+
+    public int[] dep(int posAct, int i, int j)
+    {
+        _base = posAct;
+        _pos1 = i;
+        _pos2 = j;
+
+        int[] ret = new int[2];
+        int temps = 0;
+
+        if ((_base + _pos1) <= (_base + _pos2))
+        {
+            //System.out.println("b");
+            ret[1] = 0;
+            if (_base < _pos1)
+            {
+                while (_base < _pos1)
+                {
+                    //System.out.println("je monte");
+                    temps += 4;
+                    _base++;
+                }
+            }
+            else if (_base > _pos1)
+            {
+                while (_base > _pos1)
+                {
+                    //System.out.println("je décends");
+                    temps += 2;
+                    _base--;
+                }
+            }
+            else if (_base == _pos1)
+            {
+                temps += 1;
+            }
+        }
+        else
+        {
+            //System.out.println("d");
+            ret[1] = 1;
+            if (_base < _pos2)
+            {
+                while (_base < _pos2)
+                {
+                    //System.out.println("je monte");
+                    temps += 4;
+                    _base++;
+                }
+            }
+            else if (_base > _pos2)
+            {
+                while (_base > _pos2)
+                {
+                    //System.out.println("je décends");
+                    temps += 2;
+                    _base--;
+                }
+            }
+            else if (_base == _pos2)
+            {
+                temps += 1;
+            }
+        }
+        ret[0] = temps;
+        return ret;
+    }
+
 }
--- 24hi/src/RetroGaming.java
+++ 24hi/src/RetroGaming.java
@@ -1,2 +1,80 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class RetroGaming {
+
+    public static void main(String args[])
+    {
+
+        int[][] tab = new int[3000][4000];
+        int time = 0;
+
+        try
+        {
+            // Le fichier d'entrée
+            File file = new File("src/course3.txt");
+            // Créer l'objet File Reader
+            Scanner scanner = new Scanner(file);
+
+            //renvoie true s'il y a une autre ligne à lire
+            int i = 0;
+            int j = 0;
+            while(scanner.hasNext())
+            {
+                if (j < 4000) {
+                    tab[i][j] = Integer.parseInt(scanner.next());
+                    j++;
+                }
+                else {
+                    j = 0;
+                    i++;
+                }
+            }
+            scanner.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        /*System.out.println(tab[0][0]);
+        System.out.println(tab[0][1]);
+        System.out.println(tab[0][2]);*/
+
+        Dep d = new Dep();
+
+        int k = 0;
+        int l = 0;
+        while ((k < 3000) && (l < 4000))
+        {
+            if (k == 2999)
+            {
+                time += d.depD(tab[k][l], tab[k][l+1]);
+                l++;
+            }
+            else if (l == 3999)
+            {
+                time += d.depB(tab[k][l], tab[k+1][l]);
+                k++;
+            }
+            else {
+                int[] truc = d.dep(tab[k][l], tab[k + 1][l], tab[k][l + 1]);
+                time += truc[0];
+                if (truc[1] == 0)
+                {
+                    k++;
+                }
+                else {
+                    l++;
+                }
+            }
+            System.out.println(time);
+            //System.out.println("k : " + k);               1457560
+            //System.out.println("l : " + l);
+        }
+    }
+
+    //16J 20H 52M 40S
+
+
 }
